46d7b351925f242e8d43e18b489792ccaf814bb4
Bartek Bułat <barthez@users.noreply.github.com>
2023-12-23 21:58:38 +0100

    Fix for HA 2022.12+
    The async_setup_platforms method was removed in 2022.12: https://developers.home-assistant.io/blog/2022/07/08/config_entry_forwards/

--- custom_components/fs1pg/__init__.py
+++ custom_components/fs1pg/__init__.py
@@ -22,7 +22,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     _LOGGER.debug("Using device_mac: %s", device_mac)
 
     hass.data.setdefault(DOMAIN, {})
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
